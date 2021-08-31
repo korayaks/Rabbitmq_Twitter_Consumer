9b42a8011e7c85dc7329ee852dbdbcac6ce30ba6
Koray Aksoy <burakkorayaksoy@hotmail.com>
2021-08-31 15:41:30 +0300

    Add project files.

---
+++ Rabbitmq_Consumer/Program.cs
@@ -0,0 +1,20 @@
+﻿using RabbitMQ.Client;
+using System;
+
+namespace Rabbitmq_Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connFactory = new ConnectionFactory { HostName = "localhost", Port = 5672 };
+            using (var connection = connFactory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    TopicExchangeConsumer.Consume(channel);
+                }
+            }
+        }
+    }
+}
---
+++ Rabbitmq_Consumer/TopicExchangeConsumer.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rabbitmq_Consumer
+{
+    public static class TopicExchangeConsumer
+    {
+        public static void Consume(IModel channel)
+        {
+            channel.ExchangeDeclare("social-topic-exchange", ExchangeType.Topic);//Declare Exchange name , type, and arguments.
+            channel.QueueDeclare("social-topic-queue-twitter",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);//Declare queue.
+            channel.QueueBind("social-topic-queue-twitter", "social-topic-exchange", "twitter.*");//Bind queue with Exchange and Declare routeKey.
+            channel.BasicQos(0, 10, false);
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, e) =>
+            {//Message received event
+                var body = e.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);//incoming message.
+                Console.WriteLine(message);
+            };
+            channel.BasicConsume("social-topic-queue-twitter", true, consumer);
+            Console.WriteLine("consumer started");
+            Console.ReadLine();
+        }
+    }
+}
